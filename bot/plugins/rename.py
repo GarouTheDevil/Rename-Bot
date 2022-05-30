4747beae8c387af73df20331c5dfd466d2bb7c22
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-05-30 21:13:02 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -29,27 +29,26 @@ from bot.core.handlers.big_rename import handle_big_rename
 async def rename_handler(c: Client, m: Message):
     # Checks
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Not For You ")
     if m.from_user.id not in Config.PRO_USERS:
         is_in_gap, sleep_time = await check_time_gap(m.from_user.id)
         if is_in_gap:
-            await m.reply_text("Sorry Sir,\n"
-                               "No Flooding Allowed!\n\n"
-                               f"Send After `{str(sleep_time)}s` !!",
+            await m.reply_text("No Dual Task\n"
+                               f"Wait For : `{str(sleep_time)}s`",
                                quote=True)
             return
     await add_user_to_database(c, m)
     if (not m.reply_to_message) or (not m.reply_to_message.media) or (not get_file_attr(m.reply_to_message)):
-        return await m.reply_text("Reply to any document/video/audio to rename it!", quote=True)
+        return await m.reply_text("**Reply To Media**", quote=True)
 
     # Proceed
-    editable = await m.reply_text("Now send me new file name!", quote=True)
+    editable = await m.reply_text("**Send Next Task**", quote=True)
     user_input_msg: Message = await c.listen(m.chat.id)
     if user_input_msg.text is None:
-        await editable.edit("Process Cancelled!")
+        await editable.edit("**Process Cancelled**")
         return await user_input_msg.continue_propagation()
     if user_input_msg.text and user_input_msg.text.startswith("/"):
-        await editable.edit("Process Cancelled!")
+        await editable.edit("**Process Cancelled**")
         return await user_input_msg.continue_propagation()
     _raw_file_name = get_media_file_name(m.reply_to_message)
     if not _raw_file_name:
@@ -59,7 +58,7 @@ async def rename_handler(c: Client, m: Message):
         file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + _raw_file_name.rsplit(".", 1)[-1].lower()
     else:
         file_name = user_input_msg.text[:255]
-    await editable.edit("Please Wait ...")
+    await editable.edit("**Processing...⏳**")
     is_big = get_media_file_size(m.reply_to_message) > (10 * 1024 * 1024)
     if not is_big:
         _default_thumb_ = await db.get_thumbnail(m.from_user.id)
@@ -81,17 +80,17 @@ async def rename_handler(c: Client, m: Message):
             file_name=file_name,
             progress=progress_for_pyrogram,
             progress_args=(
-                "Uploading ...\n"
+                "**Uploading ⬆️**\n"
                 f"DC: {_c_file_id.dc_id}",
                 editable,
                 c_time
             )
         )
         if not file_id:
-            return await editable.edit("Failed to Rename!\n\n"
-                                       "Maybe your file corrupted :(")
+            return await editable.edit("**Process Failed**\n"
+                                       "**Check The File**")
         await handle_big_rename(c, m, file_id, file_name, editable, file_type)
     except Exception as err:
-        await editable.edit("Failed to Rename File!\n\n"
-                            f"**Error:** `{err}`\n\n"
-                            f"**Traceback:** `{traceback.format_exc()}`")
+        await editable.edit("**Failed to Rename File**\n"
+                            f"**Error :** `{err}`\n\n"
+                            f"**Traceback :** `{traceback.format_exc()}`")
