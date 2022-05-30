6ad3eb70d2aa50ca7eb284c3f9cb6d45b9f1c1b8
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-05-30 21:18:20 +0530

    Update thumbnail.py

--- bot/plugins/thumbnail.py
+++ bot/plugins/thumbnail.py
@@ -14,8 +14,8 @@ async def show_thumbnail(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     thumbnail = await db.get_thumbnail(m.from_user.id)
     if not thumbnail:
-        return await m.reply_text("You didn't set custom thumbnail!")
-    await c.send_photo(m.chat.id, thumbnail, caption="Custom Thumbnail",
+        return await m.reply_text("**No Thumbnail Found**")
+    await c.send_photo(m.chat.id, thumbnail, caption="**Custom Thumbnail**",
                        reply_markup=types.InlineKeyboardMarkup(
                            [[types.InlineKeyboardButton("Delete Thumbnail",
                                                         callback_data="deleteThumbnail")]]
@@ -25,13 +25,12 @@ async def show_thumbnail(c: Client, m: "types.Message"):
 @Client.on_message(filters.command("set_thumbnail") & filters.private & ~filters.edited)
 async def set_thumbnail(c: Client, m: "types.Message"):
     if (not m.reply_to_message) or (not m.reply_to_message.photo):
-        return await m.reply_text("Reply to any image to save in as custom thumbnail!")
+        return await m.reply_text("**Reply To Image**")
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Not For You")
     await add_user_to_database(c, m)
     await db.set_thumbnail(m.from_user.id, m.reply_to_message.photo.file_id)
-    await m.reply_text("Okay,\n"
-                       "I will use this image as custom thumbnail.",
+    await m.reply_text("Thumbnail Will Be Saved",
                        reply_markup=types.InlineKeyboardMarkup(
                            [[types.InlineKeyboardButton("Delete Thumbnail",
                                                         callback_data="deleteThumbnail")]]
@@ -41,8 +40,7 @@ async def set_thumbnail(c: Client, m: "types.Message"):
 @Client.on_message(filters.command("delete_thumbnail") & filters.private & ~filters.edited)
 async def delete_thumbnail(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Not For You ")
     await add_user_to_database(c, m)
     await db.set_thumbnail(m.from_user.id, None)
-    await m.reply_text("Okay,\n"
-                       "I deleted custom thumbnail from my database.")
+    await m.reply_text("**Thumbnail Deleted**")
