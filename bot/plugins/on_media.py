a2ff4742839f573a16438aa3d381a3b777973f21
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-05-30 20:57:32 +0530

    Update on_media.py

--- bot/plugins/on_media.py
+++ bot/plugins/on_media.py
@@ -14,12 +14,12 @@ from pyrogram import (
 @Client.on_message((filters.video | filters.audio | filters.document) & ~filters.channel & ~filters.edited)
 async def on_media_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Not For You")
     await add_user_to_database(c, m)
     await asyncio.sleep(3)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="**Should I show File Information?**",
+        text="**Should I Show File Information ?**",
         reply_markup=types.InlineKeyboardMarkup(
             [[types.InlineKeyboardButton("Yes", callback_data="showFileInfo"),
               types.InlineKeyboardButton("No", callback_data="closeMessage")]]
